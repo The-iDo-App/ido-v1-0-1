e03a78f4a448b0d887c4ecad1d95a2788f218074
marxchryz <delmundo_marxchryz@yahoo.com>
2022-02-10 16:44:48 +0800

    Message Inbox done

---
+++ app/components/MessageOverview/index.js
@@ -0,0 +1,87 @@
+//import liraries
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import COLORS from '../../src/consts/color';
+
+const { width, height, fontScale } = Dimensions.get('window');
+
+// create a component
+const MessageOverview = ({ ProfilePicture, Name, LatestMessage, Time, id }) => {
+  return (
+    <TouchableOpacity
+      style={{ backgroundColor: 'white' }}
+      onPress={() => {
+        alert(1);
+      }}
+    >
+      <View style={{ ...styles.container }}>
+        <Image style={styles.image} source={ProfilePicture} />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            paddingLeft: 10,
+          }}
+        >
+          <View
+            style={{
+              justifyContent: 'space-around',
+              width: width - 160,
+            }}
+          >
+            <Text style={styles.title}>{Name}</Text>
+            <Text>
+              {LatestMessage.length > 30
+                ? LatestMessage.substring(0, 30) + '...'
+                : LatestMessage}
+            </Text>
+          </View>
+          <View style={{ alignSelf: 'flex-end' }}>
+            <Text>{Time}</Text>
+          </View>
+        </View>
+      </View>
+      <View
+        style={{
+          borderBottomColor: COLORS.grey,
+          borderBottomWidth: 1,
+        }}
+      />
+    </TouchableOpacity>
+  );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+  image: {
+    width: 55,
+    height: 55,
+    borderRadius: 55,
+  },
+  container: {
+    paddingHorizontal: 10,
+    marginVertical: 15,
+    width: width - 20,
+    flexDirection: 'row',
+    alignSelf: 'center',
+  },
+  title: {
+    color: COLORS.blue,
+    fontSize: 16 / fontScale,
+    fontWeight: '700',
+  },
+  debug: {
+    backgroundColor: 'yellow',
+    borderWidth: 5,
+  },
+});
+
+//make this component available to the app
+export default MessageOverview;
--- app/screens/main-screens/MessageInbox.js
+++ app/screens/main-screens/MessageInbox.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, Text } from 'react-native';
 import COLORS from '../../src/consts/color';
 import HeaderWrapper from '../../components/Header';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import MessageOverview from '../../components/MessageOverview';
+import fakeMessageOverviews from '../../models/fakeMessageOverviews';
 
 export default function MessageInbox() {
   return (
-    <SafeAreaView style={{flex: 1}}  >
-        <HeaderWrapper />
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: COLORS.white}}>
-          <Text>Inbox</Text>
-        </View>
-     </SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <HeaderWrapper />
+      <ScrollView style={{ overflow: 'scroll' }}>
+        {fakeMessageOverviews.map(
+          ({ key, profile_picture, name, latest_message, time, id }) => {
+            return (
+              <MessageOverview
+                key={key}
+                ProfilePicture={profile_picture}
+                Name={name}
+                LatestMessage={latest_message}
+                Time={time}
+              />
+            );
+          }
+        )}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
