784e745333ca2cf32dff82f72d9972854614036f
marxchryz <delmundo_marxchryz@yahoo.com>
2022-02-17 15:06:07 +0800

    get blocked users added

--- app/components/BlockedUser/index.js
+++ app/components/BlockedUser/index.js
@@ -4,46 +4,81 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Avatar } from 'react-native-paper';
 import COLORS from '../../src/consts/color';
 
+const { width } = Dimensions.get('window');
 
-const {width} = Dimensions.get('window');
-
-
-export default function BlockedUserComponent({onPress, image, nickname, email}) {
+export default function BlockedUserComponent({
+  onPress,
+  image,
+  nickname,
+  email,
+}) {
   return (
-    <View style={style.wrapper} >
-            <View style={{display: 'flex', flexDirection: 'row', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'space-around'}}  >
-                <View  style={{display: 'flex', flexDirection: 'row', alignItems: 'center', flexWrap: 'wrap',}}  >
-                    <Avatar.Image size={60} source={image} />
-                    <View  style={{display: 'flex', marginLeft: 10}}  >
-                        <Text style={{fontSize: 16, color: COLORS.blue, fontWeight: '700'}}  >{nickname}</Text>
-                        <Text style={{fontSize: 14, color: COLORS.grey, fontWeight: '300'}}  >{email}</Text>
-                    </View>
-                </View>
-               
-               <TouchableOpacity style={{padding: 10, borderColor: COLORS.blue, borderWidth: 1, borderRadius: 10  }} onPress={onPress} >
-                   <Text style={{fontSize: 16, color: COLORS.grey, fontWeight: '300'}} >UNBLOCK</Text>
-               </TouchableOpacity>
-            </View>
-           
-     </View>
+    <View style={style.wrapper}>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          flexWrap: 'wrap',
+          justifyContent: 'space-around',
+        }}
+      >
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            flexWrap: 'wrap',
+          }}
+        >
+          <Avatar.Image size={60} source={image} />
+          <View style={{ display: 'flex', marginLeft: 10 }}>
+            <Text
+              style={{ fontSize: 16, color: COLORS.blue, fontWeight: '700' }}
+            >
+              {nickname}
+            </Text>
+            <Text
+              style={{ fontSize: 14, color: COLORS.grey, fontWeight: '300' }}
+            >
+              {email}
+            </Text>
+          </View>
+        </View>
+
+        <TouchableOpacity
+          style={{
+            padding: 10,
+            borderColor: COLORS.blue,
+            borderWidth: 1,
+            borderRadius: 10,
+          }}
+          onPress={onPress}
+        >
+          <Text style={{ fontSize: 16, color: COLORS.grey, fontWeight: '300' }}>
+            UNBLOCK
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
 
-
 const style = StyleSheet.create({
-    wrapper: {
-        padding: 14 ,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3,
-        elevation: 3, 
-        backgroundColor: 'white',
-        width: width - 40, 
-        alignSelf: 'center', 
-        borderRadius: 20, 
-    }
+  wrapper: {
+    padding: 14,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3,
+    elevation: 3,
+    backgroundColor: 'white',
+    width: width - 40,
+    alignSelf: 'center',
+    borderRadius: 20,
+    marginBottom: 10,
+  },
 });
--- app/screens/main-screens/settings/BlockedUsers.js
+++ app/screens/main-screens/settings/BlockedUsers.js
@@ -1,34 +1,71 @@
-import React,{useContext} from 'react';
-import { View, Text,TouchableOpacity, Pressable} from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity, Pressable } from 'react-native';
 import COLORS from '../../../src/consts/color';
-import {FontAwesome,} from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 import { AuthContext } from '../../../components/context';
 import { ScrollView } from 'react-native-gesture-handler';
 import HeaderWrapper from '../../../components/Header';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import SettingsMainTitle from '../../../components/settingsTitle';
 import BlockedUserComponent from '../../../components/BlockedUser';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { BACKEND_DEVURL } from '@env';
+import axios from 'axios';
 
+export default function BlockedUsersScreen({ navigation }) {
+  const [blockedUsers, setBlockedUsers] = useState([]);
 
-export default function BlockedUsersScreen({navigation}) {
+  useEffect(async () => {
+    const access_token = await AsyncStorage.getItem('access_token');
+    let res = await axios.get(`${BACKEND_DEVURL}/api/settings/blocked-users`, {
+      headers: { authorization: access_token },
+    });
+
+    setBlockedUsers(res.data.users);
+  }, []);
+
   return (
-     <>
-      <View  style={{backgroundColor: 'white'}}>
+    <>
+      <View style={{ backgroundColor: 'white' }}>
         <HeaderWrapper />
-        <View style={{display: 'flex', flexDirection: 'row', marginHorizontal: 20, marginTop: 10, }} >
-              <Pressable  onPress={()=>navigation.goBack()} style={{paddingHorizontal: 10, paddingVertical: 10}} >
-                  <FontAwesome  name='angle-left' size={30} style={{color: COLORS.grey}} />
-              </Pressable>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            marginHorizontal: 20,
+            marginTop: 10,
+          }}
+        >
+          <Pressable
+            onPress={() => navigation.goBack()}
+            style={{ paddingHorizontal: 10, paddingVertical: 10 }}
+          >
+            <FontAwesome
+              name="angle-left"
+              size={30}
+              style={{ color: COLORS.grey }}
+            />
+          </Pressable>
         </View>
-     </View>
-     <View style={{flex: 1,  backgroundColor: COLORS.white}}  >
+      </View>
+      <View style={{ flex: 1, backgroundColor: COLORS.white }}>
         <ScrollView>
-            <SafeAreaView style={{marginBottom: 20}}  >
-                 <SettingsMainTitle title="Blocked Users" des={null} />
-                <BlockedUserComponent image={require('../../../src/assets/daniel_padilla.jpg')} nickname="DJ" email="djp@gmail.com" onPress={() => console.log('unblocked')} />
-            </SafeAreaView>
+          <SafeAreaView style={{ marginBottom: 20 }}>
+            <SettingsMainTitle title="Blocked Users" des={null} />
+            {blockedUsers.map((user) => {
+              return (
+                <BlockedUserComponent
+                  image={{ uri: user.profile.picture.originalImage }}
+                  key={user._id}
+                  nickname={user.username}
+                  email={user.email}
+                  onPress={() => console.log(user._id)}
+                />
+              );
+            })}
+          </SafeAreaView>
         </ScrollView>
-     </View>
+      </View>
     </>
   );
 }
