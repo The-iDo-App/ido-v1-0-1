938f19ea198660cc756c77023ff45b655cd4f409
marxchryz <delmundo_marxchryz@yahoo.com>
2022-02-21 00:13:32 +0800

    bio and avatar display added

--- app/components/interestChips/index.js
+++ app/components/interestChips/index.js
@@ -5,167 +5,174 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import COLORS from '../../src/consts/color';
 import MyFakeData from '../../models/MyData';
 
-const {width} = Dimensions.get('window');
+const { width } = Dimensions.get('window');
 
-export default function InterestChips({interest}) {
+export default function InterestChips({ interest }) {
   return (
-    <View style={{paddingHorizontal: 20, marginTop: 10, flex: 1, justifyContent: 'center' }}  >
-        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center',  justifyContent: 'center' }}  >
-             <Text style={{fontSize: 20,  color: COLORS.grey, fontWeight: '600'}} >My interests</Text>
-             <View style={{height: 1, backgroundColor: COLORS.grey, flex: 1, }} />
+    <View
+      style={{
+        paddingHorizontal: 20,
+        marginTop: 10,
+        flex: 1,
+        justifyContent: 'center',
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Text style={{ fontSize: 20, color: COLORS.grey, fontWeight: '600' }}>
+          My interests
+        </Text>
+        <View style={{ height: 1, backgroundColor: COLORS.grey, flex: 1 }} />
+      </View>
+      <View
+        style={{
+          borderColor: COLORS.grey,
+          borderWidth: 0.5,
+          borderStyle: 'solid',
+          borderRadius: 10,
+          flex: 1,
+          paddingHorizontal: 10,
+          paddingVertical: 20,
+          width: width - 40,
+          marginTop: 20,
+        }}
+      >
+        {/*SPORTS*/}
+        <View>
+          <Text style={chipStyle.itemLabel}>Sports</Text>
+          <View style={chipStyle.itemWrapper}>
+            {MyFakeData.map((item, i) => (
+              <React.Fragment key={i}>
+                {interest.sports.map((sporty, index) => (
+                  <React.Fragment key={index}>
+                    <IdoChip isProfile={false} text={sporty} isProfile={true} />
+                  </React.Fragment>
+                ))}
+              </React.Fragment>
+            ))}
+          </View>
         </View>
-        <View style={{ borderColor: COLORS.grey, borderWidth: 0.5, borderStyle: 'solid', borderRadius: 10, flex: 1, paddingHorizontal: 10, paddingVertical: 20, width: width-40, marginTop: 20}} >
-                {/*SPORTS*/}
-                <View>
-                    <Text style={chipStyle.itemLabel}>Sports</Text>
-                    <View style={chipStyle.itemWrapper} >
-                        {
-                            MyFakeData.map((item, i) => (
-                                <React.Fragment key={i}>
-                                    {
-                                        interest.sports.map((sporty, index) => (
-                                            <React.Fragment key={index}>
-                                                <IdoChip isProfile={false} text={sporty} isProfile={true} />
-                                            </React.Fragment>
-                                        ))
-                                    }
-                                </React.Fragment>
-                            ))
-                        }
-                    </View>
-                </View>
 
-                {/*HOBBIES*/}
-                <View>
-                    <Text style={chipStyle.itemLabel}>Hobbies</Text>
-                    <View style={chipStyle.itemWrapper} >
-                        {
-                            MyFakeData.map((item, i) => (
-                                <React.Fragment key={i}>
-                                    {
-                                            interest.hobbies.map((hobby, index) => (
-                                            <React.Fragment key={index}>
-                                                <IdoChip isProfile={false} text={hobby} isProfile={true} />
-                                            </React.Fragment>
-                                        ))
-                                    }
-                                </React.Fragment>
-                            ))
-                        }
-                    </View>
-                </View>
-                {/*MUSIC*/}
-                <View>
-                    <Text style={chipStyle.itemLabel}>Music taste</Text>
-                    <View style={chipStyle.itemWrapper} >
-                        {
-                            MyFakeData.map((item, i) => (
-                                <React.Fragment key={i}>
-                                    {
-                                        interest.musicGenre.map((favMusic, index) => (
-                                            <React.Fragment key={index}>
-                                                <IdoChip isProfile={false} text={favMusic} isProfile={true} />
-                                            </React.Fragment>
-                                        ))
-                                    }
-                                </React.Fragment>
-                            ))
-                        }
-                    </View>
-                </View>
-                {/*FILM*/}
-                <View>
-                    <Text style={chipStyle.itemLabel}>Film types</Text>
-                    <View style={chipStyle.itemWrapper} >
-                        {
-                            MyFakeData.map((item, i) => (
-                                <React.Fragment key={i}>
-                                    {
-                                        interest.movieGenre.map((filmy, index) => (
-                                            <React.Fragment key={index}>
-                                                <IdoChip isProfile={false} text={filmy} isProfile={true} />
-                                            </React.Fragment>
-                                        ))
-                                    }
-                                </React.Fragment>
-                            ))
-                        }
-                    </View>
-                </View>
-                {/*PET*/}
-                <View>
-                    <Text style={chipStyle.itemLabel}>Pets I like</Text>
-                    <View style={chipStyle.itemWrapper} >
-                        {
-                            MyFakeData.map((item, i) => (
-                                <React.Fragment key={i}>
-                                    {
-                                        interest.pets.map((pets, index) => (
-                                            <React.Fragment key={index}>
-                                                <IdoChip isProfile={false} text={pets} isProfile={true} />
-                                            </React.Fragment>
-                                        ))
-                                    }
-                                </React.Fragment>
-                            ))
-                        }
-                    </View>
-                </View>
-                {/*BOOK*/}
-                <View>
-                    <Text style={chipStyle.itemLabel}>Books that I like</Text>
-                    <View style={chipStyle.itemWrapper} >
-                        {
-                            MyFakeData.map((item, i) => (
-                                <React.Fragment key={i}>
-                                    {
-                                        interest.books.map((booky, index) => (
-                                            <React.Fragment key={index}>
-                                                <IdoChip isProfile={false} text={booky} isProfile={true} />
-                                            </React.Fragment>
-                                        ))
-                                    }
-                                </React.Fragment>
-                            ))
-                        }
-                    </View>
-                </View>
-                {/*FOOD*/}
-                <View>
-                    <Text style={chipStyle.itemLabel}>Foods I like</Text>
-                    <View style={chipStyle.itemWrapper} >
-                        {
-                            MyFakeData.map((item, i) => (
-                                <React.Fragment key={i}>
-                                    {
-                                        interest.food.map((foodies, index) => (
-                                            <React.Fragment key={index}>
-                                                <IdoChip isProfile={false} text={foodies} isProfile={true} />
-                                            </React.Fragment>
-                                        ))
-                                    }
-                                </React.Fragment>
-                            ))
-                        }
-                    </View>
-                </View>
-
+        {/*HOBBIES*/}
+        <View>
+          <Text style={chipStyle.itemLabel}>Hobbies</Text>
+          <View style={chipStyle.itemWrapper}>
+            {MyFakeData.map((item, i) => (
+              <React.Fragment key={i}>
+                {interest.Hobbies.map((hobby, index) => (
+                  <React.Fragment key={index}>
+                    <IdoChip isProfile={false} text={hobby} isProfile={true} />
+                  </React.Fragment>
+                ))}
+              </React.Fragment>
+            ))}
+          </View>
         </View>
-     </View>
+        {/*MUSIC*/}
+        <View>
+          <Text style={chipStyle.itemLabel}>Music taste</Text>
+          <View style={chipStyle.itemWrapper}>
+            {MyFakeData.map((item, i) => (
+              <React.Fragment key={i}>
+                {interest.musicGenre.map((favMusic, index) => (
+                  <React.Fragment key={index}>
+                    <IdoChip
+                      isProfile={false}
+                      text={favMusic}
+                      isProfile={true}
+                    />
+                  </React.Fragment>
+                ))}
+              </React.Fragment>
+            ))}
+          </View>
+        </View>
+        {/*FILM*/}
+        <View>
+          <Text style={chipStyle.itemLabel}>Film types</Text>
+          <View style={chipStyle.itemWrapper}>
+            {MyFakeData.map((item, i) => (
+              <React.Fragment key={i}>
+                {interest.movieGenre.map((filmy, index) => (
+                  <React.Fragment key={index}>
+                    <IdoChip isProfile={false} text={filmy} isProfile={true} />
+                  </React.Fragment>
+                ))}
+              </React.Fragment>
+            ))}
+          </View>
+        </View>
+        {/*PET*/}
+        <View>
+          <Text style={chipStyle.itemLabel}>Pets I like</Text>
+          <View style={chipStyle.itemWrapper}>
+            {MyFakeData.map((item, i) => (
+              <React.Fragment key={i}>
+                {interest.pets.map((pets, index) => (
+                  <React.Fragment key={index}>
+                    <IdoChip isProfile={false} text={pets} isProfile={true} />
+                  </React.Fragment>
+                ))}
+              </React.Fragment>
+            ))}
+          </View>
+        </View>
+        {/*BOOK*/}
+        <View>
+          <Text style={chipStyle.itemLabel}>Books that I like</Text>
+          <View style={chipStyle.itemWrapper}>
+            {MyFakeData.map((item, i) => (
+              <React.Fragment key={i}>
+                {interest.books.map((booky, index) => (
+                  <React.Fragment key={index}>
+                    <IdoChip isProfile={false} text={booky} isProfile={true} />
+                  </React.Fragment>
+                ))}
+              </React.Fragment>
+            ))}
+          </View>
+        </View>
+        {/*FOOD*/}
+        <View>
+          <Text style={chipStyle.itemLabel}>Foods I like</Text>
+          <View style={chipStyle.itemWrapper}>
+            {MyFakeData.map((item, i) => (
+              <React.Fragment key={i}>
+                {interest.food.map((foodies, index) => (
+                  <React.Fragment key={index}>
+                    <IdoChip
+                      isProfile={false}
+                      text={foodies}
+                      isProfile={true}
+                    />
+                  </React.Fragment>
+                ))}
+              </React.Fragment>
+            ))}
+          </View>
+        </View>
+      </View>
+    </View>
   );
 }
 
 const chipStyle = StyleSheet.create({
-  itemWrapper:{
-    display: 'flex', 
-    flexDirection: 'row', 
-    width: '100%',  
-    flexWrap: 'wrap'
+  itemWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    flexWrap: 'wrap',
   },
   itemLabel: {
-    color: COLORS.blue, 
-    fontSize: 16, 
+    color: COLORS.blue,
+    fontSize: 16,
     fontWeight: '700',
-    marginVertical: 10
-  }
-})
+    marginVertical: 10,
+  },
+});
--- app/screens/main-screens/Profile.js
+++ app/screens/main-screens/Profile.js
@@ -1,5 +1,11 @@
-import React,{useState,useEffect} from 'react';
-import { View, Text, Dimensions, StyleSheet,TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import COLORS from '../../src/consts/color';
 import HeaderWrapper from '../../components/Header';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -11,124 +17,125 @@ import AboutMe from '../../components/AboutMe';
 import MyAnswers from '../../components/EvaluationMyselfChips';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
-import {BACKEND_BASEURL,BACKEND_DEVURL,PORT} from '@env';
+import { BACKEND_BASEURL, BACKEND_DEVURL, PORT } from '@env';
 import Loading from '../../components/ActivityIndicator';
-const {width} = Dimensions.get('window');
+const { width } = Dimensions.get('window');
 
-export default function Profile({navigation}) {
+export default function Profile({ navigation }) {
   const [evalsInfo, setEvalsInfo] = useState(null);
   const [userInfo, setUserInfo] = useState(null);
   const [loading, setLoading] = useState(false);
-  
-  const fetchUser = async()=>{
-    const userId = await AsyncStorage.getItem("userId");
-    const access_token = await AsyncStorage.getItem("access_token");
+
+  const fetchUser = async () => {
+    const userId = await AsyncStorage.getItem('userId');
+    const access_token = await AsyncStorage.getItem('access_token');
 
     const config = {
       headers: {
-            "Content-type": "application/json",
-            "Authorization": `${access_token}`,
-      }
-    }
+        'Content-type': 'application/json',
+        Authorization: `${access_token}`,
+      },
+    };
     let user;
-    try{
-      user = await axios.post(`${BACKEND_BASEURL}/api/profiles`,{userId},config);
-    }catch(err){
+    try {
+      user = await axios.post(
+        `${BACKEND_BASEURL}/api/profiles`,
+        { userId },
+        config
+      );
+    } catch (err) {
       console.log(err);
     }
-    if(user.data.user){
+    if (user.data.user) {
       setUserInfo(user.data);
       setLoading(true);
+      // console.log(user.data);
     }
     // console.log(userInfo);
-  }
-  const fetchEvals = async()=>{
-    const userId = await AsyncStorage.getItem("userId");
+  };
+  const fetchEvals = async () => {
+    const userId = await AsyncStorage.getItem('userId');
     let evals;
-    try{
+    try {
       evals = await axios.get(`${BACKEND_BASEURL}/api/evaluations/${userId}`);
-      console.log(evals);
-    }catch(err){
+      // console.log(evals);
+    } catch (err) {
       console.log(err);
     }
-    if(evals.data.questions){
+    if (evals.data.questions) {
       setEvalsInfo(evals.data);
     }
     // console.log(evalsInfo);
-  }
+  };
 
   useEffect(() => {
-    fetchUser();  
+    fetchUser();
     fetchEvals();
-  },[loading]);
+  }, [loading]);
 
   return (
     <>
       <HeaderWrapper />
-        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}  >
-          <ScrollView >
-            {userInfo !== null && evalsInfo !== null?(<View style={{marginVertical: 20}} >
-                {/*profile photo, name and city*/}
-                <View style={style.contentWrapper} >
-                    <View style={style.profilePhotoWrapper}  >
-                        <UploadProfile />
-                    </View>
-                    <View style={style.nameWrapper} >
-                        <Text style={style.nameText}>{userInfo.user.firstName}</Text>
-                        <Text style={style.cityText}>{userInfo.user.address.city}</Text>
-                    </View>
+      <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
+        <ScrollView>
+          {userInfo !== null && evalsInfo !== null ? (
+            <View style={{ marginVertical: 20 }}>
+              {/*profile photo, name and city*/}
+              <View style={style.contentWrapper}>
+                <View style={style.profilePhotoWrapper}>
+                  <UploadProfile picture={userInfo.profile.picture} />
                 </View>
-                <BioComponent />
-                <AboutMe about={userInfo.interest}/>
-                <InterestChips interest={userInfo.interest}/>
-                {/*Yung onPress prop ng MyAnswers ay mapupunta sa Question screen para magdagdag ng answers*/}
-                <MyAnswers evaluation={evalsInfo}/>
-            </View>)
-          :(
-              <Loading />
+                <View style={style.nameWrapper}>
+                  <Text style={style.nameText}>{userInfo.user.firstName}</Text>
+                  <Text style={style.cityText}>
+                    {userInfo.user.address.city}
+                  </Text>
+                </View>
+              </View>
+              <BioComponent bio={userInfo.profile.shortDescription} />
+              <AboutMe about={userInfo.interest} />
+              <InterestChips interest={userInfo.interest} />
+              {/*Yung onPress prop ng MyAnswers ay mapupunta sa Question screen para magdagdag ng answers*/}
+              <MyAnswers evaluation={evalsInfo} />
+            </View>
+          ) : (
+            <Loading />
           )}
-            
-            
-            
-          </ScrollView>
-        </SafeAreaView>
+        </ScrollView>
+      </SafeAreaView>
     </>
-    
   );
 }
 
 //TODO: CHANGE BACKEND URL
 
-
-
 const style = StyleSheet.create({
-    contentWrapper: {
-      display: 'flex', 
-      flexDirection: 'row', 
-      alignItems: 'center', 
-      justifyContent: 'center', 
-      width: width/1.15,
-       marginTop: 20
-    },
-    profilePhotoWrapper:{
-      display: 'flex', 
-      flexDirection: 'column', 
-      alignItems: 'flex-start', 
-      marginRight: 20
-    },
-    nameWrapper:{
-      display: 'flex', 
-      flexDirection: 'column', 
-      alignItems: 'flex-start', 
-    },
-    nameText:{
-      fontSize: 30, 
-      color: COLORS.blue, 
-      fontWeight: '700'
-    },
-    cityText:{
-      fontSize: 16, 
-      color: COLORS.grey
-    }
-
+  contentWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: width / 1.15,
+    marginTop: 20,
+  },
+  profilePhotoWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    marginRight: 20,
+  },
+  nameWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  nameText: {
+    fontSize: 30,
+    color: COLORS.blue,
+    fontWeight: '700',
+  },
+  cityText: {
+    fontSize: 16,
+    color: COLORS.grey,
+  },
 });
